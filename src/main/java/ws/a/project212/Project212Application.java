1a7f3019fbe691d8ce83d092d8126c011f283d1f
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-12-05 10:02:05 +0700

    first commit

---
+++ src/main/java/ws/a/project212/Project212Application.java
@@ -0,0 +1,13 @@
+package ws.a.project212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project212Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/a/project212/Project212ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.project212;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project212ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
